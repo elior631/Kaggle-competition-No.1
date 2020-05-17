7033217b40499e68e366a43bba3d5dfa7da07b54
Elior Bliah <elior.bliah@mail.huji.ac.il>
2020-05-17 14:23:58 +0300

    VISUALIZATION!

--- new script.R
+++ new script.R
@@ -1,5 +1,5 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(DataExplorer,readr, glmnet, tidymodels,kableExtra, stargazer,ROCit, caret)
+pacman::p_load(DataExplorer,readr,plyr, glmnet, tidymodels,kableExtra, stargazer,ROCit, caret)
 
 #Load train and test data
 urlfile1= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/master/train.csv"
@@ -7,3 +7,30 @@ urlfile2= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/ma
 
 train_set<- read_csv(url(urlfile1))
 test_set<- read_csv(url(urlfile2))
+
+#Define sex as factor
+train_set$female <- as.factor(train_set$female)
+
+# visualization of the data
+
+# DataExplorer::create_report(train_set)
+
+
+train_set %>% 
+  ggplot(aes(x = edyrs, y = lnwage, color = female)) + 
+  scale_color_discrete(name = "Sex") +
+  xlab('Years of education')+
+  ylab('Salary log')+
+  geom_point()
+
+log_sex_mean <- ddply(train_set, "female", summarise, grp.mean=mean(lnwage))
+
+train_set %>%
+  ggplot(aes(x = lnwage, fill = female)) +
+  geom_density(alpha = 0.4) +
+  geom_vline(data = log_sex_mean, aes(xintercept=grp.mean, color=female),
+              linetype="twodash")+
+  xlab("Log of wage")
+
+
+
