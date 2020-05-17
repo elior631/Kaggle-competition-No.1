d75849cc88873fdd0bf744c07f2952fe9434c958
Elior Bliah <elior.bliah@mail.huji.ac.il>
2020-05-17 13:27:13 +0300

    upload packages and load and save data

--- new script.R
+++ new script.R
@@ -1,4 +1,9 @@
-library(tidymodels)
-library(dataExplore)
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(DataExplorer,readr, glmnet, tidymodels,kableExtra, stargazer,ROCit, caret)
 
-jh
+#Load train and test data
+urlfile1= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/master/train.csv"
+urlfile2= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/master/test.csv"
+
+train_set<- read_csv(url(urlfile1))
+test_set<- read_csv(url(urlfile2))
