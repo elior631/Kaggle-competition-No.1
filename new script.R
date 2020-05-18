3454ef84bfec7171ca474ce500891bd3119af4e9
Elior Bliah <elior.bliah@mail.huji.ac.il>
2020-05-18 16:07:06 +0300

    more visualizations

--- new script.R
+++ new script.R
@@ -1,5 +1,5 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(DataExplorer,readr,plyr, glmnet, tidymodels,kableExtra, stargazer,ROCit, caret)
+pacman::p_load(DataExplorer,readr,plyr,gridExtra, glmnet, tidymodels,kableExtra, stargazer,ROCit, caret)
 
 #Load train and test data
 urlfile1= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/master/train.csv"
@@ -8,8 +8,10 @@ urlfile2= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/ma
 train_set<- read_csv(url(urlfile1))
 test_set<- read_csv(url(urlfile2))
 
-#Define sex as factor
-train_set$female <- as.factor(train_set$female)
+
+# Define as factors all binary features
+vec <- c(3, 8:39)
+train_set[vec]<- lapply(train_set[vec], factor)
 
 # visualization of the data
 
@@ -23,15 +25,56 @@ train_set %>%
   ylab('Log of wage')+
   geom_point()
 
+# lnwage distribution by sex
 log_sex_mean <- ddply(train_set, "female", summarise, grp.mean=mean(lnwage))
 
-train_set %>%
+sex_p <- train_set %>%
   ggplot(aes(x = lnwage, fill = female)) +
-  geom_density(alpha = 0.4) +
+  geom_density(alpha = 0.3) +
   geom_vline(data = log_sex_mean, aes(xintercept = grp.mean),
               linetype="twodash") +
   xlab("Log of wage") +
   scale_fill_brewer(palette="Dark2")
 
+# lnwage distribution by black/white
+log_black_mean <- ddply(train_set, "black", summarise, grp.mean=mean(lnwage))
 
+black_p<- train_set %>%
+  ggplot(aes(x = lnwage, fill= black)) +
+  geom_density(alpha = 0.3) +
+  geom_vline(data = log_black_mean, aes(xintercept = grp.mean),
+             linetype="twodash") +
+  xlab("Log of wage") +
+  scale_fill_brewer(palette="Dark2")
 
+# lnwage distribution by hispanic
+
+log_hisp_mean <- ddply(train_set, "hisp", summarise, grp.mean=mean(lnwage))
+
+hisp_p <- train_set %>%
+  ggplot(aes(x = lnwage, fill= hisp)) +
+  geom_density(alpha = 0.3) +
+  geom_vline(data = log_hisp_mean, aes(xintercept = grp.mean),
+             linetype="twodash") +
+  xlab("Log of wage") +
+  scale_fill_brewer(palette="Dark2")
+
+# lnwage distribution by college degree
+
+log_colldeg_mean <- ddply(train_set, "colldeg", summarise, grp.mean=mean(lnwage))
+
+colldeg_p <- train_set %>%
+  ggplot(aes(x = lnwage, fill = colldeg)) +
+  geom_density(alpha = 0.3) +
+  geom_vline(data = log_colldeg_mean, aes(xintercept = grp.mean),
+             linetype="twodash")
+  xlab("Log of wage") +
+  scale_fill_brewer(palette="Dark2")
+  
+  
+  # Plot all graph together
+  
+
+  grid.arrange(colldeg_p, hisp_p, black_p, sex_p)
+
+    
