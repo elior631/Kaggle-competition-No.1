a17431bdadaf71f84a290baa51f527defb533784
Elior Bliah <elior.bliah@mail.huji.ac.il>
2020-05-20 19:36:18 +0300

    done with models

---
+++ boosting.R
@@ -0,0 +1,69 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(DataExplorer,here, readr,plyr,gridExtra, glmnet, tidymodels,kableExtra, stargazer,ROCit, caret)
+
+#Load train and test data
+urlfile1= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/master/train.csv"
+urlfile2= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/master/test.csv"
+
+train_set<- read_csv(url(urlfile1))
+test_set<- read_csv(url(urlfile2))
+
+
+train_set<- train_set %>%
+  mutate(fem_edu = female*edyrs, fem_col = female*colldeg,
+         fem_bl = female*black, bl_edu =black*edyrs, bl_col = black*colldeg,
+         edu2 = edyrs*edyrs) 
+
+
+test_set<- test_set %>%
+  mutate(fem_edu = female*edyrs, fem_col = female*colldeg,
+         fem_bl = female*black, bl_edu =black*edyrs, bl_col = black*colldeg,
+         edu2 = edyrs*edyrs) 
+
+set.seed(1994)
+data_split<- initial_split(train_set, prop = 0.7)
+train_set1 <- training(data_split)
+test_set1 <- testing(data_split)
+
+# Fit a model
+
+x_train <- train_set1[, c(3:45)] %>%
+  data.matrix()
+y_train <- train_set1$lnwage
+
+x_test <- test_set1[, c(3:45)] %>%
+  data.matrix()
+y_test <- test_set1$lnwage  
+
+test_set_noID <- data.matrix(test_set[, c(2:44)])
+
+
+formula_full <- lnwage ~ . -ID
+
+fitControl <- trainControl(method = "repeatedcv",
+                           number = 5,
+                           repeats = 3)
+gbm <- train(formula_full,
+             data = train_set1,
+             method = "gbm",
+             trControl = fitControl)
+
+models_pred <- data.frame(
+  gbm = predict(gbm, newdata = test_set1),
+  truth = test_set1$lnwage)
+
+rmse(models_pred, truth, gbm)
+
+pred<- predict(gbm, newdata = test_set) 
+
+to_print <- data.frame( test_set$ID, pred) %>%
+  rename( "lnwage" = pred )
+
+
+
+write.csv(
+  to_print,
+  file = ("E_Lior6_gbm.csv"),
+  row.names = FALSE
+)
+
--- new script.R
+++ new script.R
@@ -6,20 +6,20 @@ urlfile1= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/ma
 urlfile2= "https://raw.githubusercontent.com/elior631/Kaggle-competition-No.1/master/test.csv"
 
 train_set<- read_csv(url(urlfile1))
-test_set<- read_csv(url(urlfile2)) %>%
-  data.matrix()
-
+test_set<- read_csv(url(urlfile2))
 
 # Define as factors all binary features
-vec <- c(3, 8:39)
-train_set[vec]<- lapply(train_set[vec], factor)
+vec <- c(3, 8:45)
+train_set_viz <- train_set
+train_set_viz[vec]<- lapply(train_set_viz[vec], factor)
 
-# visualization of the data
+#########################################################
+#visualization of the data
 
 # DataExplorer::create_report(train_set)
 
-
-train_set %>% 
+if (FALSE){
+train_set_viz %>% 
   ggplot(aes(x = edyrs, y = lnwage, color = female)) + 
   scale_color_discrete(name = "Sex") +
   xlab('Years of education')+
@@ -27,9 +27,9 @@ train_set %>%
   geom_point()
 
 # lnwage distribution by sex
-log_sex_mean <- ddply(train_set, "female", summarise, grp.mean=mean(lnwage))
+log_sex_mean <- ddply(train_set_viz, "female", summarise, grp.mean=mean(lnwage))
 
-sex_p <- train_set %>%
+sex_p <- train_set_viz %>%
   ggplot(aes(x = lnwage, fill = female)) +
   geom_density(alpha = 0.3) +
   geom_vline(data = log_sex_mean, aes(xintercept = grp.mean),
@@ -38,9 +38,9 @@ sex_p <- train_set %>%
   scale_fill_brewer(palette="Dark2")
 
 # lnwage distribution by black/white
-log_black_mean <- ddply(train_set, "black", summarise, grp.mean=mean(lnwage))
+log_black_mean <- ddply(train_set_viz, "black", summarise, grp.mean=mean(lnwage))
 
-black_p<- train_set %>%
+black_p<- train_set_viz %>%
   ggplot(aes(x = lnwage, fill= black)) +
   geom_density(alpha = 0.3) +
   geom_vline(data = log_black_mean, aes(xintercept = grp.mean),
@@ -50,9 +50,9 @@ black_p<- train_set %>%
 
 # lnwage distribution by hispanic
 
-log_hisp_mean <- ddply(train_set, "hisp", summarise, grp.mean=mean(lnwage))
+log_hisp_mean <- ddply(train_set_viz, "hisp", summarise, grp.mean=mean(lnwage))
 
-hisp_p <- train_set %>%
+hisp_p <- train_set_viz %>%
   ggplot(aes(x = lnwage, fill= hisp)) +
   geom_density(alpha = 0.3) +
   geom_vline(data = log_hisp_mean, aes(xintercept = grp.mean),
@@ -62,9 +62,9 @@ hisp_p <- train_set %>%
 
 # lnwage distribution by college degree
 
-log_colldeg_mean <- ddply(train_set, "colldeg", summarise, grp.mean=mean(lnwage))
+log_colldeg_mean <- ddply(train_set_viz, "colldeg", summarise, grp.mean=mean(lnwage))
 
-colldeg_p <- train_set %>%
+colldeg_p <- train_set_viz %>%
   ggplot(aes(x = lnwage, fill = colldeg)) +
   geom_density(alpha = 0.3) +
   geom_vline(data = log_colldeg_mean, aes(xintercept = grp.mean),
@@ -78,8 +78,26 @@ colldeg_p <- train_set %>%
 # גרפים לא אחידים לסדר
   grid.arrange(colldeg_p, hisp_p, black_p, sex_p)
 
-    
-# The model
+} 
+  
+  
+  
+#########################################################
+
+  #The model
+
+
+  train_set<- train_set %>%
+    mutate(fem_edu = female*edyrs, fem_col = female*colldeg,
+           fem_bl = female*black, bl_edu =black*edyrs, bl_col = black*colldeg,
+           edu2 = edyrs*edyrs) 
+
+
+test_set<- test_set %>%
+  mutate(fem_edu = female*edyrs, fem_col = female*colldeg,
+         fem_bl = female*black, bl_edu =black*edyrs, bl_col = black*colldeg,
+         edu2 = edyrs*edyrs) 
+  
   set.seed(1994)
   data_split<- initial_split(train_set, prop = 0.7)
   train_set1 <- training(data_split)
@@ -87,24 +105,24 @@ colldeg_p <- train_set %>%
   
 # Fit a model
 
-  x_train <- train_set1[, c(3:39)] %>%
+  x_train <- train_set1[, c(3:45)] %>%
     data.matrix()
     y_train <- train_set1$lnwage
     
-    x_test <- test_set1[, c(3:39)] %>%
+    x_test <- test_set1[, c(3:45)] %>%
       data.matrix()
     y_test <- test_set1$lnwage  
     
-    test_set_noID <- data.matrix(test_set[, c(2:38)])
-
-  
-  fit_Lasso <- glmnet(x_train, y_train, alpha = 0.5)
+    test_set_noID <- data.matrix(test_set[, c(2:44)])
+    
+    
+  fit_Lasso <- glmnet(x_train, y_train, alpha = 0)
   fit_Lasso %>% 
     plot(xvar = "lambda")
   
   # Picking the best Lambda
   
-  cv_fit <- cv.glmnet(x_train, y_train, alpha = 0.5)
+  cv_fit <- cv.glmnet(x_train, y_train, alpha = 0)
   cv_fit %>%
     plot()
   
@@ -120,20 +138,23 @@ colldeg_p <- train_set %>%
 
 
    multi_metric <- metric_set(rmse, rsq, mae)
-   multi_metric(data = test_set1, truth = y_test, estimate = pred_cv_1se) %>%
+   multi_metric(data = test_set1, truth = y_test, estimate = pred_cv_min) %>%
      tibble() %>%
      mutate(.metric = c("RMSE", "R-Squared", "MAE"))
    
    
-   pred_cv_1se <- as.vector(predict(cv_fit, s = "lambda.1se",
+   pred_cv_1se_test <- as.vector(predict(cv_fit, s = "lambda.1se",
                                     newx = test_set_noID))
 
-   results <- data.frame(test_set[,1], pred_cv_1se)
+   results <- data.frame(test_set[,1], pred_cv_1se_test) %>%
+     rename( "lnwage" = pred_cv_1se_test )
+     
   
    write.csv(
      results ,
-    file = ("E_Lior2.csv"),
+    file = ("E_Lior4.csv"),
      row.names = FALSE
    )
+   
    
    
